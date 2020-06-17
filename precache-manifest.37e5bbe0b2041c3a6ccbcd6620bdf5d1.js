285569970e8db95008fe4e4d1503884a616d9364
Toine Hulshof <me@toinehulshof.com>
2020-06-17 03:00:43 +0200

    Initial commit

---
+++ precache-manifest.37e5bbe0b2041c3a6ccbcd6620bdf5d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd54e81a4c3b9fa9e9fbdcef0c4fc26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "841b94a24661bf4a524f",
+    "url": "/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "48c2c7cc33f42ffb9c81",
+    "url": "/static/css/main.78d0fc29.chunk.css"
+  },
+  {
+    "revision": "841b94a24661bf4a524f",
+    "url": "/static/js/2.8a824e28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8a824e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48c2c7cc33f42ffb9c81",
+    "url": "/static/js/main.c15f3419.chunk.js"
+  },
+  {
+    "revision": "9fb118e61b5ffb3c0175",
+    "url": "/static/js/runtime-main.2914d6a2.js"
+  }
+]);
